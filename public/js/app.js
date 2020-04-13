047d7ad8015ab52306f6c3951b2ca3a775ed0303
Srivathsan S <srivathsansenthil16@gmail.com>
2020-04-13 11:19:31 +0530

    Unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('client side server is up')
-
 const weatherForm=document.querySelector('form')
 const search=document.querySelector('input')
 const messageOne=document.querySelector('#message-one')
